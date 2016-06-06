f70eeee27231fc326a7614b02a0cbd7ed69d2be5
William Goulart <williamgoulart1@hotmail.com>
2016-06-06 08:31:39 -0300

    Add styles in main.scss and make fn ConvertToF

--- js/code.js
+++ js/code.js
@@ -1,1 +1,14 @@
-console.log(teste);
+let weatherType = document.querySelector('.weather-type');
+let weatherMeasure = document.querySelector('.measure');
+let celsius = weatherMeasure.innerHTML;
+let fahrenheit = convertToF(celsius);
+
+function convertToF(celsius) {
+  wheaterF = celsius * (9/5) + (32);
+  return wheaterF;
+}
+
+weatherType.addEventListener('click', function(){
+  weatherMeasure.innerHTML = convertToF(celsius);
+  weatherType.innerHTML = "F";
+});
