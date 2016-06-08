e3c81bfbfb62eca205315de095b2c4e600372516
William Goulart <williamgoulart1@hotmail.com>
2016-06-08 05:23:20 -0400

    update changes/ create new js

--- js/code.js
+++ js/code.js
@@ -13,8 +13,7 @@ function convertToC(fahrenheit) {
   return celsius;
 }
 
-var getPosition = function (data) {
-
+var getPosition = function(data) {
   // converter date
   let currentData = new Date(data.dt * 1000);
   day = ('0' + currentData.getDate()).slice(-2);
@@ -29,18 +28,19 @@ var getPosition = function (data) {
   weatherF = convertToC(fahrenheit);
 }
 
-
 if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function (position) {
+  navigator.geolocation.getCurrentPosition(function(position) {
     var posLatitude = position.coords.latitude;
     var posLongitude = position.coords.longitude;
 
-    var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + posLatitude + "&lon=" + posLongitude + "&appid=5fb4f48349670d743ffb765da99e7262&units=metric";
+    var url = "http://api.openweathermap.org/data/2.5/weather?lat=" +
+      posLatitude + "&lon=" + posLongitude +
+      "&appid=5fb4f48349670d743ffb765da99e7262&units=metric";
     $.getJSON(url, getPosition, 'json');
   });
 }
 
-weatherType.addEventListener('click', function () {
+weatherType.addEventListener('click', function() {
   if (weatherType.innerHTML == "C") {
     weatherMeasure.innerHTML = fahrenheit;
     weatherType.innerHTML = "F";
---
+++ js/getLocation.js
@@ -0,0 +1,72 @@
+let urlLocationIp = 'http://ip-api.com/json/'; // get location for ip
+let confs = {};
+let weatherLocation = document.querySelector('.weather-location');
+let weatherMeasure = document.querySelector('.measure');
+let weatherType = document.querySelector('.weather-type');
+let weatherDate = document.querySelector('.weather-date');
+
+// init values
+confs.celsius = 0;
+confs.fahrenheit = 0;
+
+function getLocation(data) {
+    confs.posLat = data.lat;
+    confs.posLong = data.lon;
+    confs.city = data.city;
+    confs.regionName = data.region;
+    confs.url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + confs.posLat + '&lon=' + confs.posLong + '&appid=5fb4f48349670d743ffb765da99e7262&units=metric';
+    getWeather(confs.url);
+}
+
+if (navigator.geolocation) {
+    fetch(urlLocationIp, {
+        method: 'get'
+    }).then(function(response) {
+        response.json().then(function(result) {
+            getLocation(result);
+        })
+    }).catch(function(err) {
+        console.error(err);
+    });
+} else {
+    console.log("Your Browser not supported geolocation api. Atualiza ele MANO!");
+}
+
+function getWeather(url) {
+    fetch(url, {
+        method: 'get'
+    }).then(function(response) {
+        response.json().then(function(result) {
+            getResult(result);
+        })
+    }).catch(function(err) {
+        console.error(err);
+    });
+}
+
+function getResult(weather) {
+    weatherMeasure.innerHTML = weather.main.temp;
+    weatherLocation.innerHTML = weather.name + ' - ' + confs.regionName;
+    confs.celsius = weatherMeasure.innerHTML;
+    confs.fahrenheit = convertToF(confs.celsius);
+}
+
+function convertToF(celsius) {
+    let fahrenheit = (celsius * 1.8000) + (32);
+    return fahrenheit;
+}
+
+function convertToC(fahrenheit) {
+    let celsius = fahrenheit * (9 / 5) + (32);
+    return celsius;
+}
+
+weatherType.addEventListener('click', function() {
+    if (weatherType.innerHTML === "C") {
+        weatherMeasure.innerHTML = confs.fahrenheit;
+        weatherType.innerHTML = "F";
+    } else {
+        weatherMeasure.innerHTML = confs.celsius;
+        weatherType.innerHTML = "C";
+    }
+});
